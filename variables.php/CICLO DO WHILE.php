3be7bc6698d4c41e5a255ccfe359e62808f54253
Said Suarez Corzo <102996921+ElSaidSuarez@users.noreply.github.com>
2022-07-18 16:07:53 -0500

    Add files via upload

---
+++ variables.php/CICLO DO WHILE.php
@@ -0,0 +1,16 @@
+<?php
+
+echo"Do while";
+echo"<br/>";
+
+$numero = 0;
+do {
+    echo $numero. "<br/>";
+    $numero = $numero + 1;
+
+}while ($numero < 10);
+
+
+
+
+?>
---
+++ variables.php/CICLO WHILE.php
@@ -0,0 +1,19 @@
+<?php
+
+echo "ciclo while";
+echo "<br/>";
+
+// CICLO WHILE....
+$numero = 0;
+
+while( $numero < 10) {
+
+    echo "el numero actual es: " . $numero;
+    $numero = $numero + 1;
+
+}
+
+echo "<br/>";
+echo "finalizo...";
+
+?>
